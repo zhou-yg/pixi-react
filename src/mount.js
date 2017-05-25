cb66b74bf02c37c298ab8b3064fb5e2e9639ffac
zhouyg <haraldred@163.com>
2017-05-25 20:26:14 +0800

    updater

---
+++ src/mount.js
@@ -0,0 +1,35 @@
+import * as utils from './utils.js';
+const {isUndef, isDef,log} = utils;
+
+var i = 0;
+export function mountComponent(node, parentComponent) {
+  const instance = new node.type(node.props, node.slots);
+  const vNode = instance.render();
+
+  node.instance = instance;
+
+  if(utils.isPixiObj(vNode)){
+    instance.pixiEl = vNode;
+    instance.isMounted = true;
+    parentComponent.pixiEl.addChild(vNode);
+
+  } else if(utils.isVNode(vNode)){
+
+    instance.vNode = vNode;
+    instance.pixiEl = parentComponent.pixiEl;
+    instance.isMounted = true;
+
+    const rootInstance = mountComponent(vNode, instance);
+
+  }else{
+    throw new Error('mountComponent 卧槽');
+  }
+
+  node.children.map(childNode => {
+
+    const childInstance = mountComponent(childNode, instance);
+    instance.children.push(childInstance);
+  });
+
+  return instance;
+}
